b9fb97b9f466a4550b6ccdd8401a593478990353
aaron <alkrauss48@gmail.com>
2021-06-18 06:17:49 -0500

    Initial Commit

---
+++ CyrusLinks/CyrusLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyrusLinksApp.swift
+//  CyrusLinks
+//
+//  Created by Aaron Krauss on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct CyrusLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
