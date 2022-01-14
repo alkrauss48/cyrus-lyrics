89c584a075d7135e520dddf2bad686b316dbb76f
aaron <alkrauss48@gmail.com>
2022-01-14 12:44:41 -0600

    Add initial slide out menu

--- CyrusLyrics/Pages/ContentView.swift
+++ CyrusLyrics/Pages/ContentView.swift
@@ -9,26 +9,90 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var dataManager = DataManager()
-
+    @State var menuOpen: Bool = false
+    
     var body: some View {
-        NavigationView {
-            List(dataManager.categories, id: \.id) { category in
-                NavigationLink(destination: CategoryDetailView(category: category)) {
-                    Text(category.name)
+        ZStack {
+            NavigationView {
+                List(dataManager.categories, id: \.id) { category in
+                    NavigationLink(destination: CategoryDetailView(category: category)) {
+                        Text(category.name)
+                    }
+                }.refreshable {
+                    dataManager.queryAppData()
                 }
-            }.refreshable {
-                dataManager.queryAppData()
+                .navigationTitle("Categories")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: {
+                            self.openMenu()
+                        }) {
+                            Image(systemName: "line.horizontal.3")
+                        }
+                    }
+                    ShuffleToolbarItem(type: "all", id: nil)
+                }
             }
-            .navigationTitle("Categories")
-            .toolbar {
-                ShuffleToolbarItem(type: "all", id: nil)
-            }
+            
+            SideMenu(width: 270,
+                    isOpen: self.menuOpen,
+                    menuClose: self.openMenu)
         }
+    }
+    
+    func openMenu() {
+        self.menuOpen.toggle()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MenuContent: View {
+    var body: some View {
+        List {
+            Text("My Profile").onTapGesture {
+                print("My Profile")
+            }
+            Text("Posts").onTapGesture {
+                print("Posts")
+            }
+            Text("Logout").onTapGesture {
+                print("Logout")
+            }
+        }
+    }
+}
+
+struct SideMenu: View {
+    let width: CGFloat
+    let isOpen: Bool
+    let menuClose: () -> Void
+    
+    var body: some View {
+        ZStack {
+            GeometryReader { _ in
+                EmptyView()
+            }
+            .background(Color.gray.opacity(0.3))
+            .opacity(self.isOpen ? 1.0 : 0.0)
+            .animation(Animation.easeIn.delay(0.25))
+            .onTapGesture {
+                self.menuClose()
+            }
+            
+            HStack {
+                MenuContent()
+                    .frame(width: self.width)
+                    .background(Color.white)
+                    .offset(x: self.isOpen ? 0 : -self.width)
+                    .animation(.default)
+                
+                Spacer()
+            }
+        }
     }
 }
