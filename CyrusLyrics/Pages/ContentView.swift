bcbbc0bfdfb8f04ef301d3f8c4ad4ae83b9a6811
aaron <alkrauss48@gmail.com>
2022-03-09 21:48:47 -0600

    Force navigation style as stack To force iPad to be the same as iPhone

--- CyrusLyrics/Pages/ContentView.swift
+++ CyrusLyrics/Pages/ContentView.swift
@@ -25,6 +25,7 @@ struct ContentView: View {
                     isOpen: stateManager.menuOpen,
                     menuClose: stateManager.toggleMenu)
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
     
 
